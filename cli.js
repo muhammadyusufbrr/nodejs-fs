b133cc571227158e8ecfdef995c67ce8651b8d26
muhammadyusufbrr <muhammadyusufdeveloper@gmail.com>
2025-11-08 22:05:42 +0500

    cli

---
+++ cli.js
@@ -0,0 +1,9 @@
+const logger = () => {
+  const resp = {};
+  for (let i = 0; i < process.argv.length; i++) {
+    const arg = process.argv[i].split("=");
+    resp[arg[0]] = arg[1] ? arg[1] : true;
+  }
+  return resp;
+};
+console.log(logger());
